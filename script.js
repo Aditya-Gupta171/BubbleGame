37760fffc4032cb8d0febc674fa1075a992c79f4
Aditya-Gupta171 <adityagupta14april@gmail.com>
2024-01-12 00:40:33 +0530

    updation in js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var pichlavalue="";
+function bubblebanao(){
+    var pichlavalue="";
 
 for(var i=1;i<=102;i++){
-    pichlavalue+=`<div class="bubble">1</div>`
+    var rndmnum=Math.floor(Math.random()*10);
+    pichlavalue+=`<div class="bubble">${rndmnum}</div>`;
 }
 document.querySelector("#pbtm").innerHTML=pichlavalue;
+}
+
+var timer=4;
+function runtimer(){
+  var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timervalue").textContent=timer;
+        }
+        else{
+            clearInterval(timerint);
+        }
+    },1000);}
+
+bubblebanao();
+runtimer();
