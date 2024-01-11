75b85037f479baa10b396b2fbe7644fbf73a9623
Aditya-Gupta171 <adityagupta14april@gmail.com>
2024-01-12 03:16:18 +0530

    fixed event bubbling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var timer=60;
+var timer=5;
 var score=0;
+var hitrunner=0;
 function bubblebanao(){
     var pichlavalue="";
-
 for(var i=1;i<=102;i++){
     var rndmnum=Math.floor(Math.random()*10);
     pichlavalue+=`<div class="bubble">${rndmnum}</div>`;
@@ -17,15 +17,28 @@ function runtimer(){
         }
         else{
             clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML=`<h1>GAME OVER <h1>`;
         }
     },1000);}
- function getnewhit(){
-    var hitrunner=Math.floor(Math.random()*10);
+ function getNewHit(){
+     hitrunner=Math.floor(Math.random()*10);
     document.querySelector("#hitvalue").textContent=hitrunner;
  }
 function increasescore(){
-    
+    score+=10;
+    document.querySelector("#scorevalue").textContent=score;
 }
-bubblebanao();
+
+document.querySelector("#pbtm")
+.addEventListener("click", function(dets){
+    var clickednum=(Number(dets.target.textContent));
+    if(clickednum === hitrunner){
+        increasescore();
+        bubblebanao();
+        getNewHit();
+    }
+})
+
 runtimer();
-getnewhit();
+bubblebanao();
+getNewHit();
